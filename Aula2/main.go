9e7c33ae827b6b54cfd79483c094c1e7b0711589
moronibr <moroni.ramos@gmail.com>
2024-08-29 15:13:56 -0300

    Second

--- Aula2/main.go
+++ Aula2/main.go
@@ -1,8 +1,61 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var env = "dev"
+var cache map[string]*template.Template
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "index")
+}
+
+func ContactHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "contact")
+}
+
+func RenderTemplate(w http.ResponseWriter, page string) {
+
+	var t *template.Template
+	var err error
+
+	_, exists := cache[page]
+
+	if !exists || env == "dev" {
+		t, err = template.Must(template.ParseFiles("templates/"+page+"Template.html")).ParseFiles(
+			"templates/"+page+"Template.html",
+			"templates/baseTemplate.html",
+		)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		cache[page] = t
+	} else {
+		fmt.Println("Cache hit")
+		t = cache[page]
+	}
+
+	t.Execute(w, nil)
+}
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	cache = make(map[string]*template.Template)
+
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/contact", ContactHandler)
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		RenderTemplate(w, "about")
+	})
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
 	http.ListenAndServe(":3000", nil)
 }
